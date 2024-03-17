dce7771cf969ed9031f4e7f6a7be7cc3b21a92e8
ngtdungdev <dungboi1029@gmail.com>
2024-03-18 00:31:30 +0700

    Initial commit

---
+++ app/src/main/java/com/example/qr_code/qrcode/QRCodeActivity.java
@@ -0,0 +1,6 @@
+package com.example.qr_code.qrcode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class QRCodeActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/example/qr_code/scanQrcode/CameraActivity.java
@@ -0,0 +1,7 @@
+package com.example.qr_code.scanQrcode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CameraActivity extends AppCompatActivity {
+
+}
