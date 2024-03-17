be182c69efde17f1b67d6a9edd2fa73c5082d112
ngtdungdev <dungboi1029@gmail.com>
2024-03-18 01:04:03 +0700

    qrcode

--- app/src/main/java/com/example/qr_code/qrcode/QRCodeActivity.java
+++ app/src/main/java/com/example/qr_code/qrcode/QRCodeActivity.java
@@ -1,6 +1,55 @@
 package com.example.qr_code.qrcode;
 
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.qr_code.R;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
 public class QRCodeActivity extends AppCompatActivity {
+    private EditText textView;
+    private Button btnTextQrCode;
+    private ImageView imageViewQRCode;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_code);
+        textView = findViewById(R.id.urlText);
+        btnTextQrCode = findViewById(R.id.btnQrcode);
+        imageViewQRCode = findViewById(R.id.imageQrcode);
+        btnTextQrCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                QRCodeWriter writer = new QRCodeWriter();
+                BitMatrix bitMatrix = null;
+                try {
+                    bitMatrix = writer.encode( textView.getText().toString(), BarcodeFormat.QR_CODE, 240, 260);
+                } catch (WriterException e) {
+                    throw new RuntimeException(e);
+                }
+                int width = bitMatrix.getWidth();
+                int height = bitMatrix.getHeight();
+                Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+                for (int x = 0; x < width; x++) {
+                    for (int y = 0; y < height; y++) {
+                        bmp.setPixel(x, y, bitMatrix.get(x, y) ? 0xFF000000 : 0xFF00BFFF);
+                    }
+                }
+                imageViewQRCode.setImageBitmap(bmp);
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/example/qr_code/scanQrcode/CameraActivity.java
+++ app/src/main/java/com/example/qr_code/scanQrcode/CameraActivity.java
@@ -1,7 +1,76 @@
 package com.example.qr_code.scanQrcode;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class CameraActivity extends AppCompatActivity {
+import com.example.qr_code.R;
+import com.journeyapps.barcodescanner.BarcodeCallback;
+import com.journeyapps.barcodescanner.BarcodeResult;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+import com.journeyapps.barcodescanner.Size;
 
+import java.util.regex.Pattern;
+
+public class CameraActivity extends AppCompatActivity {
+    private Button btResult;
+    private DecoratedBarcodeView barcodeView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_qr_code);
+        scanCode();
+    }
+    private void scanCode()
+    {
+        barcodeView = findViewById(R.id.barcode_scanner);
+        btResult = findViewById(R.id.btResult);
+        barcodeView.decodeContinuous(callback);
+        barcodeView.getBarcodeView().setFramingRectSize(new Size(720,1300));
+        barcodeView.setStatusText("");
+        barcodeView.getViewFinder().setVisibility(View.INVISIBLE);
+        barcodeView.resume();
+        btResult.setEnabled(false);
+        btResult.setText("Scan a QR Code");
+        btResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (function) {
+                    case 1 : {
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(saveText));
+                        startActivity(intent);
+                    } break;
+                    default: {
+                        Toast.makeText(CameraActivity.this, saveText, Toast.LENGTH_SHORT).show();
+                        btResult.setEnabled(false);
+                    }
+                }
+                finish();
+            }
+        });
+
+    }
+    private String saveText;
+    private int function = 0;
+    private BarcodeCallback callback = new BarcodeCallback() {
+        @Override
+        public void barcodeResult(BarcodeResult result) {
+            if (result.getText() != null) {
+                saveText = result.getText();
+                if (Pattern.compile("(https?://)?(www\\.)?[a-zA-Z0-9-]+(\\.[a-zA-Z]{2,})(:[0-9]+)?(/\\S*)?").matcher(saveText).find()) {
+                    btResult.setText("ADD CONTENT TO THE URL");
+                    function = 3;
+                }
+                btResult.setEnabled(true);
+                barcodeView.pause();
+            }
+        }
+
+    };
 }
